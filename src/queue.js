f9c2e68b00ba7808f30ffe19f4b5095e8ddca7b5
Vasili <avazingg@gmail.com>
2023-12-21 22:30:49 +0100

    feat: stack and queue done

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.queue = {};
+    this.head = 0;
+    this.tail = 0;
+
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queue[this.head]
+    //return this.queue
+  }
+
+  enqueue(value) {
+    if (this.tail == 0) {
+      const item = new Node(value);
+      this.queue[this.tail] = item
+    }
+    else {
+      let currrent = this.queue[this.head]
+      while (currrent.next) {
+        currrent = currrent.next
+      }
+      currrent.next = new Node(value)
+    }
+    this.tail++
+    // return this.queue.push(value) = new Node(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let item = this.queue[this.head]
+    this.queue[this.head] = item.next
+    this.tail--
+    return item.value
+    //return this.queue.shift()
   }
+
 }
 
 module.exports = {
